b1c89342e65b3c8f1eb4367bf829ff575f03d082
erinszabo <55733370+erinszabo@users.noreply.github.com>
2022-01-23 12:03:03 -0800

    create endless (for now) loop

---
+++ mush.c
@@ -0,0 +1,17 @@
+#include<stdio.h> // for printf(), fgets(), perror()
+#include<stdlib.h> // for exit()
+#include<string.h> // for strtok(), strcmp()
+#include<unistd.h> // for fork(), execvp()
+#include<sys/wait.h> // for wait()
+
+int main() {
+    // char user_entry[];
+
+
+    for (;;){
+        // if(user_entry == "exit") {
+        //     break;
+        // }
+    }
+    return 0;
+}
